4fb5c6c079542778fb3d8c04dcd6582d8d1c176a
KiuV1 <45211476+KiuV1@users.noreply.github.com>
2018-11-20 19:04:08 +0000

    Update and rename README.md to index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
+});
+
+bot.login(NTEzNzA3MDM3NTA3NDUyOTU3.DtXpRw.Ol2Yem0eDXVpv2dfRor_fyvPoSI);
+
